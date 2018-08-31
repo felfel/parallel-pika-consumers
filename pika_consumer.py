adbe2d666dd35553fc1ac3ab398f60ce3ecaf030
Benjamin Ellenberger <benjamin@felfel.ch>
2018-08-31 16:44:33 +0200

    Shutdown gracefully

--- pika_consumer.py
+++ pika_consumer.py
@@ -410,9 +410,9 @@ if __name__ == '__main__':
             LOGGER.info("Goodbye!")
             consumer.stop()
 
+    atexit.register(shutdown_consumers)
+
     for consumer in pika_consumers:
         consumer.join()
-
-    atexit.register(shutdown_consumers)
 
     #app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
